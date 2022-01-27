db15ecca372daf21256a04306739f72dcbd33934
xDestyn <omar.flores.cs@outlook.com>
2022-01-27 00:39:06 -0600

    Update logs so they appear within logstream

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -67,8 +67,10 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
+            app.logger.warning('/login - Login Unsuccessful - Invalid credentials')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
+        app.logger.info('/login - Login Successful - Valid credentials')
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
@@ -82,6 +84,7 @@ def authorized():
     if request.args.get('state') != session.get("state"):
         return redirect(url_for("home"))  # No-OP. Goes back to Index page
     if "error" in request.args:  # Authentication/Authorization failure
+        app.logger.warning('/redirect - Login Unsuccessful - Authentication/Authorization failure');
         return render_template("auth_error.html", result=request.args)
     if request.args.get('code'):
         cache = _load_cache()
@@ -92,13 +95,13 @@ def authorized():
             redirect_uri = url_for('authorized', _external = True, _scheme = 'https')
         )
         if "error" in result:
-            app.logger.warning('Login unsuccessful')
+            app.logger.warning('/redirect - Login Unsuccessful - Invalid MSAL Authentication')
             return render_template("auth_error.html", result=result)
         session["user"] = result.get("id_token_claims")
         # Note: In a real app, we'd use the 'name' property from session["user"] below
         # Here, we'll use the admin username for anyone who is authenticated by MS
         user = User.query.filter_by(username="admin").first()
-        app.logger.info("Login successful")
+        app.logger.info("/redirect - Login Successful - Valid MSAL Authentication")
         login_user(user)
         _save_cache(cache)
     return redirect(url_for('home'))
@@ -107,13 +110,14 @@ def authorized():
 def logout():
     logout_user()
     if session.get("user"): # Used MS Login
-        app.logger.info('Logout Successful')
+        app.logger.info('/logout - Logout Successful - MSAL Logout')
         # Wipe out user and its token cache from session
         session.clear()
         # Also logout from your tenant's web session
         return redirect(
             Config.AUTHORITY + "/oauth2/v2.0/logout" +
             "?post_logout_redirect_uri=" + url_for("login", _external=True))
+    app.logger.info('/logout - Logout Successful')
     return redirect(url_for('login'))
 
 def _load_cache():
